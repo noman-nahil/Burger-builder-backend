09388ab83e56d8036398876b49037a8e2a0d89fd
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-13 23:57:12 +0600

    initial setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const app = require('./app');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_SERVER, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useCreateIndex: true,
+}).then(() => console.log("Connected to MongoDB!"))
+    .catch(err => console.error("MongoDB Connection Failed!"));
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}....`);
+});
